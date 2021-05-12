33f4dff1f989388fb5f0d245de8ac034f3654cf6
Josi yt <64885027+josephfigueroa@users.noreply.github.com>
2021-05-11 22:42:50 -0400

    Add files via upload

---
+++ DocumentosJosi/src/Main.java
@@ -0,0 +1,11 @@
+/* DocumentosJosi
+ * System.out.println("Joseph Figueroa");
+ * System.out.println("11 de mayo del 2021");
+ * System.out.println("Open source");
+ * System.out.println("Bienvenido a mi portafolio");
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hola Git Hub");
+	}
+}
